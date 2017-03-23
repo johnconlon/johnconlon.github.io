e44be49d35eee39237e6b6ba51841b3df76da552
John Otto Conlon <johnottoconlon@gmail.com>
2017-03-22 22:24:40 -0700

    Changed H1 Font and fixed header border

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 // Make it rain!
+// Add thing that tells you how far you've scrolled form the top
+// when the use scrolls past the main header image
+	// collapse the menu to left
+	// turn into hamburger 
+	// arrow up button appears on side of the screen to you to the top
+
+
+// 
