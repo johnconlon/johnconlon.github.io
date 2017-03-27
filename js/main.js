0ab39d0b9cf5f256b44100c67dca01fbf94341cc
John Otto Conlon <johnottoconlon@gmail.com>
2017-03-26 22:13:08 -0700

    Added transitions and scroll to top JS

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 // Make it rain!
 // Add thing that tells you how far you've scrolled form the top
+$(window).on('scroll', function () {
+	// Step 1: Google $(window).scrollTop();
+	var distanceScrolled = $(window).scrollTop();
+
+	console.log('The distance scrolled is: ' + distanceScrolled);
+
+	if (distanceScrolled >= 1450) {
+			$('.up-arrow').addClass('active');
+		}
+	else {
+			$('.up-arrow').removeClass('active');
+		}
+});
+
 // when the use scrolls past the main header image
 	// collapse the menu to left
 	// turn into hamburger 
@@ -7,3 +21,22 @@
 
 
 // 
+
+
+// Smooth scroll this mother!
+
+// Do it when someone clicks a nav link
+$('nav a').on('click', function(e) {
+  // prevent the standard link operation on click
+  e.preventDefault();
+  // use the href of the link to identify what
+  // section to scroll to
+  var thisTarget = $(this).attr('href');
+  // get that section's top offset
+  var targetOffset = $(thisTarget).offset().top;
+  // use jQuery.animate() to animate the body's
+  // scrollTop to the targetOffest
+  $('body').animate({
+    scrollTop: targetOffset
+  }, 600);
+});
